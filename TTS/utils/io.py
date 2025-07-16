da091e1c3312aecfa3e9583ed17d382733d268af
Jin Li <jinlmsft@hotmail.com>
2025-07-16 14:43:59 -0700

    update

--- TTS/utils/io.py
+++ TTS/utils/io.py
@@ -51,7 +51,7 @@ def load_fsspec(
             return torch.load(f, map_location=map_location, **kwargs)
     else:
         with fsspec.open(path, "rb") as f:
-            return torch.load(f, map_location=map_location, **kwargs)
+            return torch.load(f, weights_only=False, map_location=map_location, **kwargs)
 
 
 def load_checkpoint(
